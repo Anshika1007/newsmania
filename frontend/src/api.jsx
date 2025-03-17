80897402823e731583c5afc04475dc60a468c84c
Anshika1303 <anshikachugh107@gmail.com>
2025-03-17 23:46:00 +0530

    some changes made

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const BASE_URL = import.meta.env.VITE_API_BASE_URL.replace(/\/$/, "");
 
 const API_ENDPOINTS = {
     NEWS: `${BASE_URL}/news`, 
